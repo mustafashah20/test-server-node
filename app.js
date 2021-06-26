089013840f7faf1f9e1bfc637df204af66513e10
Mustafa Shah <i170253@nu.edu.pk>
2021-06-26 20:48:41 +0500

    artist endpoint removed

--- app.js
+++ app.js
@@ -11,21 +11,6 @@ app.use(
     })
 )
 
-app.get("/artist", async (req, res) => {
-    const url = `https://rest.bandsintown.com/artists/${req.query.name}?app_id=12345`
-    const options = {
-        "method": "GET",
-    }
-    const response = await fetch(url, options)
-        .then(res => res.json())
-        .catch(e => ({
-            "message": "oh no",
-            error: e,
-        })
-        )
-    res.json(response)
-});
-
 app.get("/search", async (req, res) => {
     const url = `https://www.bandsintown.com/searchSuggestions?searchTerm=${req.query.name}`
     const options = {
